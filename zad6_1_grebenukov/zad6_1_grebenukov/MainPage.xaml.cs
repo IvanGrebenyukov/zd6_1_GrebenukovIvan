59226d13ce6b941076b1bba22af619e0f5be21e2
Ivan <122042934+IvanGrebenyukov@users.noreply.github.com>
2023-06-21 13:50:49 +0500

    Add files via upload

---
+++ zad6_1_grebenukov/zad6_1_grebenukov/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zad6_1_grebenukov
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage (string login, string password)
+        {
+            InitializeComponent();
+        }
+    }
+}
